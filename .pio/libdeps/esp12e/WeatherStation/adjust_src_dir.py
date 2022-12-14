fa81c6afa45d1d3c2ee4e9b94658e58ed0ae85ea
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2022-12-13 19:19:57 -0800

    Pushing delete of files that shouldn't have been uploaded

--- .pio/libdeps/esp12e/WeatherStation/adjust_src_dir.py
+++
@@ -1,5 +0,0 @@
-import os
-
-Import("env")
-env['PROJECT_SRC_DIR'] = env['PROJECT_DIR'] + os.sep + "examples" + os.sep + env["PIOENV"]
-print("Setting the project directory to: {}".format(env['PROJECT_SRC_DIR']))
