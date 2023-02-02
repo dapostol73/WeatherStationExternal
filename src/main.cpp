370889edab86f7ac1824049c2489e38ce79c4b7b
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2023-02-01 16:26:53 -0800

    Striped out display and OpenWeather data fetch
    Trimmed down external to only be a sensor reader and upload the data to ThinkSpeak as it for external use only.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,3 @@
-#include <ESPWiFi.h>
-#include <ESPHTTPClient.h>
-#include <JsonListener.h>
-
-// time
-#include <time.h>                       // time() ctime()
-#include <sys/time.h>                   // struct timeval
-#include <coredecls.h>                  // settimeofday_cb()
-
-#include "WifiInfo.h"
-#include "SSD1306Wire.h"
-#include "OLEDDisplayUi.h"
-#include "Wire.h"
-#include "OpenWeatherMapCurrent.h"
-#include "OpenWeatherMapForecast.h"
-#include "WeatherStationFonts.h"
-#include "WeatherStationImages.h"
 #include <ESP8266WiFi.h>
 
 #include <Adafruit_Sensor.h>
@@ -23,11 +6,13 @@
 #include <DHT_U.h>
 #include <BH1750FVI.h>
 
+#include "WifiInfo.h"
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
-// Uncomment the type of sensor in use:
+WiFiConnection wiFiInfo("Unknown", "Invalid");
 
+// Uncomment the type of sensor in use:
 WiFiClient client;
 const char *host = "api.thingspeak.com";                  //IP address of the thingspeak server
 const char *api_key ="EMCNAORN3ZXKCFW1";                  //Your own thingspeak api_key
@@ -39,11 +24,11 @@ const int httpPort = 80;
 #define DHTTYP DHT22 // DHT 22 (AM2302)
 #define DHTPIN 14    // Digital pin connected to the DHT sensor 
 float tempTemp = 0.0; //temperature
-float tempHumi = 0.0; //humidity
+float tempHmd = 0.0; //humidity
 void readTemperatureHumidity();
 void uploadSensorData();
-long readTime = 0; 
-long uploadTime = 0; 
+long readTime = LONG_MIN; 
+long uploadTime = LONG_MIN; 
 
 /***************************
  * Begin Light Sensor Settings
@@ -62,15 +47,10 @@ float tempAlit = 0.0;
 /***************************
  * Begin Settings
  **************************/
-#define TZ              -8     // (utc+) TZ in hours
-#define DST_MN          0      // use 60mn for summer time in some countries
-
 // Setup
 const int SENSOR_INTERVAL_SECS = 15; // Sensor query every 15 seconds
 const int UPLOAD_INTERVAL_SECS = 5 * 60; // Upload every 5 minutes
-const int UPDATE_INTERVAL_SECS = 20 * 60; // Update every 20 minutes
-// Display Settings
-const int I2C_DISPLAY_ADDRESS = 0x3c;
+
 #if defined(ESP8266)
 const int SDA_PIN = D2;
 const int SCL_PIN = D1;
@@ -81,31 +61,6 @@ const int SDA_PIN = GPIO0;
 const int SCL_PIN = GPIO2 
 #endif
 
-
-// OpenWeatherMap Settings
-// Sign up here to get an API key:
-// https://docs.thingpulse.com/how-tos/openweathermap-key/
-const boolean IS_METRIC = true;
-// Add your own thingpulse ID 
-String OPEN_WEATHER_MAP_APP_ID = "6ef72f6a0b2b7849e74ac530ce47d067";
-String OPEN_WEATHER_MAP_LOCATION = "6090785";
-
-// Pick a language code from this list:
-// Arabic - ar, Bulgarian - bg, Catalan - ca, Czech - cz, German - de, Greek - el,
-// English - en, Persian (Farsi) - fa, Finnish - fi, French - fr, Galician - gl,
-// Croatian - hr, Hungarian - hu, Italian - it, Japanese - ja, Korean - kr,
-// Latvian - la, Lithuanian - lt, Macedonian - mk, Dutch - nl, Polish - pl,
-// Portuguese - pt, Romanian - ro, Russian - ru, Swedish - se, Slovak - sk,
-// Slovenian - sl, Spanish - es, Turkish - tr, Ukrainian - ua, Vietnamese - vi,
-// Chinese Simplified - zh_cn, Chinese Traditional - zh_tw.
-
-String OPEN_WEATHER_MAP_LANGUAGE = "en";
-const uint8_t MAX_FORECASTS = 4;
-
-// Adjust according to your language
-const String WDAY_NAMES[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-const String MONTH_NAMES[] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-
 /***************************
  * End Settings
  **************************/
@@ -114,47 +69,12 @@ DHT_Unified dht22(DHTPIN, DHTTYP);
 BH1750FVI bh1750(BH1750_DEFAULT_I2CADDR, BH1750_CONTINUOUS_HIGH_RES_MODE, BH1750_SENSITIVITY_DEFAULT, BH1750_ACCURACY_DEFAULT);
 Adafruit_BMP280 bmp280;
 
-// Initialize the oled display for address 0x3c
-SSD1306Wire     display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN);
-OLEDDisplayUi   ui( &display );
-
-OpenWeatherMapCurrentData currentWeather;
-OpenWeatherMapCurrent currentWeatherClient;
-
-OpenWeatherMapForecastData forecasts[MAX_FORECASTS];
-OpenWeatherMapForecast forecastClient;
-
-#define TZ_MN           ((TZ)*60)
-#define TZ_SEC          ((TZ)*3600)
-#define DST_SEC         ((DST_MN)*60)
-time_t now;
-
-// flag changed in the ticker function every 10 minutes
-bool readyForWeatherUpdate = false;
-String lastUpdate = "--";
-long timeSinceLastWUpdate = 0;
 //declaring prototypes
-void drawProgress(OLEDDisplay *display, int percentage, String label);
-void updateData(OLEDDisplay *display);
-void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
-void drawCurrentWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
-void drawForecast(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
-void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex);
-void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);
-void setReadyForWeatherUpdate();
-
-
-// Add frames
-// this array keeps function pointers to all frames
-// frames are the single views that slide from right to left
-FrameCallback frames[] = { drawDateTime, drawCurrentWeather, drawForecast };
-int numberOfFrames = 3;
-
-OverlayCallback overlays[] = { drawHeaderOverlay };
-int numberOfOverlays = 1;
+void resolveWiFiInfo();
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(2000);
 
   //initialize Atmosphere sensor
   if (!bmp280.begin(I2C_ATOM_ADDRESS)) {
@@ -176,50 +96,18 @@ void setup() {
   // initlialize Humidity 
   dht22.begin();
 
-  // initialize dispaly
-  display.init();
-  display.clear();
-  display.display();
-
-  //display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.setContrast(255);
-
-  WiFi.begin(NAME_OF_SSID, PASSWORD_OF_SSID);
+  WiFi.mode(WIFI_STA);
+  resolveWiFiInfo();
+  WiFi.begin(wiFiInfo.SSID, wiFiInfo.Password);
 
   int counter = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    display.clear();
-    display.drawString(64, 10, "Connecting to WiFi");
-    display.drawXbm(46, 30, 8, 8, counter % 3 == 0 ? activeSymbole : inactiveSymbole);
-    display.drawXbm(60, 30, 8, 8, counter % 3 == 1 ? activeSymbole : inactiveSymbole);
-    display.drawXbm(74, 30, 8, 8, counter % 3 == 2 ? activeSymbole : inactiveSymbole);
-    display.display();
-
     counter++;
   }
-  // Get time from network time service
-  configTime(TZ_SEC, DST_SEC, "pool.ntp.org");
-  ui.setTargetFPS(30);
-  ui.setActiveSymbol(activeSymbole);
-  ui.setInactiveSymbol(inactiveSymbole);
-  // You can change this to
-  // TOP, LEFT, BOTTOM, RIGHT
-  ui.setIndicatorPosition(BOTTOM);
-  // Defines where the first frame is located in the bar.
-  ui.setIndicatorDirection(LEFT_RIGHT);
-  // You can change the transition that is used
-  // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_TOP, SLIDE_DOWN
-  ui.setFrameAnimation(SLIDE_LEFT);
-  ui.setFrames(frames, numberOfFrames);
-  ui.setOverlays(overlays, numberOfOverlays);
-  // Inital UI takes care of initalising the display too.
-  ui.init();
+  
   Serial.println("");
-  updateData(&display);
   while (!client.connect(host, httpPort)) {
     Serial.println("Connection Failed");
   }  
@@ -227,7 +115,7 @@ void setup() {
 
 void loop() {  
   //Read sensor values base on Upload interval seconds
-  if(millis() - readTime > SENSOR_INTERVAL_SECS){
+  if(millis() - readTime > 1000L*SENSOR_INTERVAL_SECS){
     readTemperatureHumidity();
     readLight();
     readAtmosphere();
@@ -235,131 +123,38 @@ void loop() {
   }
 
   //Upload Sensor values base on Upload interval seconds
-  if(millis() - uploadTime > UPLOAD_INTERVAL_SECS){
+  if(millis() - uploadTime > 1000L*UPLOAD_INTERVAL_SECS){
     uploadSensorData();
     uploadTime = millis();
   }
-  
-  if (millis() - timeSinceLastWUpdate > (1000L*UPDATE_INTERVAL_SECS)) {
-    setReadyForWeatherUpdate();
-    timeSinceLastWUpdate = millis();
-  }
-
-  if (readyForWeatherUpdate && ui.getUiState()->frameState == FIXED) {
-    updateData(&display);
-  }
-
-  int remainingTimeBudget = ui.update();
-
-  if (remainingTimeBudget > 0) {
-    // You can do some work here
-    // Don't do stuff if you are below your
-    // time budget.
-    delay(remainingTimeBudget);
-  }
 }
 
-void drawProgress(OLEDDisplay *display, int percentage, String label) {
-  display->clear();
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(64, 10, label);
-  display->drawProgressBar(2, 28, 124, 10, percentage);
-  display->display();
-}
+void resolveWiFiInfo()
+{
+	int8_t numNetworks = WiFi.scanNetworks();
+  Serial.println("Number of networks found: " + String(numNetworks));
 
-void updateData(OLEDDisplay *display) {
-  drawProgress(display, 10, "Updating time...");
-  drawProgress(display, 30, "Updating weather...");
-  currentWeatherClient.setMetric(IS_METRIC);
-  currentWeatherClient.setLanguage(OPEN_WEATHER_MAP_LANGUAGE);
-  currentWeatherClient.updateCurrentById(&currentWeather, OPEN_WEATHER_MAP_APP_ID, OPEN_WEATHER_MAP_LOCATION);
-  drawProgress(display, 50, "Updating forecasts...");
-  forecastClient.setMetric(IS_METRIC);
-  forecastClient.setLanguage(OPEN_WEATHER_MAP_LANGUAGE);
-  uint8_t allowedHours[] = {12};
-  forecastClient.setAllowedHours(allowedHours, sizeof(allowedHours));
-  forecastClient.updateForecastsById(forecasts, OPEN_WEATHER_MAP_APP_ID, OPEN_WEATHER_MAP_LOCATION, MAX_FORECASTS);
-  readyForWeatherUpdate = false;
-  drawProgress(display, 100, "Done...");
-  delay(1000);
-}
+	for (uint8_t i=0; i<numNetworks; i++)
+	{
+		String ssid = WiFi.SSID(i);
+		Serial.println(WiFi.SSID(i) + " (" + String(WiFi.RSSI(i)) + ")");
+		for (uint8_t j=0; j < WiFiConnectionsCount; j++)
+		{
+      if (strcasecmp(WiFiConnections[j].SSID, ssid.c_str()) == 0)
+			{
+				//Serial.println("Found: " + String(ssid));
+				WiFiConnections[j].Avialable = true;
+				WiFiConnections[j].Strength = WiFi.RSSI(i);
 
+				if (WiFiConnections[j].Strength > wiFiInfo.Strength)
+				{
+					wiFiInfo = WiFiConnections[j];
+				}
+			}
+		}
+	}
 
-
-void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  now = time(nullptr);
-  struct tm* timeInfo;
-  timeInfo = localtime(&now);
-  char buff[16];
-
-
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->setFont(ArialMT_Plain_10);
-  String date = WDAY_NAMES[timeInfo->tm_wday];
-
-  sprintf_P(buff, PSTR("%s, %02d/%02d/%04d"), WDAY_NAMES[timeInfo->tm_wday].c_str(), timeInfo->tm_mday, timeInfo->tm_mon+1, timeInfo->tm_year + 1900);
-  display->drawString(64 + x, 5 + y, String(buff));
-  display->setFont(ArialMT_Plain_24);
-
-  sprintf_P(buff, PSTR("%02d:%02d:%02d"), timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
-  display->drawString(64 + x, 15 + y, String(buff));
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-}
-
-void drawCurrentWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  display->setFont(ArialMT_Plain_10);
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->drawString(64 + x, 38 + y, currentWeather.description);
-
-  display->setFont(ArialMT_Plain_24);
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  String temp = String(currentWeather.temp, 1) + (IS_METRIC ? "°C" : "°F");
-  display->drawString(60 + x, 5 + y, temp);
-
-  display->setFont(Meteocons_Plain_36);
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->drawString(32 + x, 0 + y, currentWeather.iconMeteoCon);
-}
-
-
-void drawForecast(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  drawForecastDetails(display, x, y, 0);
-  drawForecastDetails(display, x + 44, y, 1);
-  drawForecastDetails(display, x + 88, y, 2);
-}
-
-void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex) {
-  time_t observationTimestamp = forecasts[dayIndex].observationTime;
-  struct tm* timeInfo;
-  timeInfo = localtime(&observationTimestamp);
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x + 20, y, WDAY_NAMES[timeInfo->tm_wday]);
-
-  display->setFont(Meteocons_Plain_21);
-  display->drawString(x + 20, y + 12, forecasts[dayIndex].iconMeteoCon);
-  String temp = String(forecasts[dayIndex].temp, 0) + (IS_METRIC ? "°C" : "°F");
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(x + 20, y + 34, temp);
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-}
-
-void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
-  now = time(nullptr);
-  struct tm* timeInfo;
-  timeInfo = localtime(&now);
-  char buff[14];
-  sprintf_P(buff, PSTR("%02d:%02d"), timeInfo->tm_hour, timeInfo->tm_min);
-
-  display->setColor(WHITE);
-  display->setFont(ArialMT_Plain_10);
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->drawString(0, 54, String(buff));
-  display->setTextAlignment(TEXT_ALIGN_RIGHT);
-  String temp = String(currentWeather.temp, 1) + (IS_METRIC ? "°C" : "°F");
-  display->drawString(128, 54, temp);
-  display->drawHorizontalLine(0, 52, 128);
+	Serial.println("Using WiFi " + String(wiFiInfo.SSID));	
 }
 
 float roundUpDecimal(float value, int decimals = 1) {
@@ -368,9 +163,8 @@ float roundUpDecimal(float value, int decimals = 1) {
   return value;
 }
 
-void setReadyForWeatherUpdate() {
-  Serial.println("Setting readyForUpdate to true");
-  readyForWeatherUpdate = true;
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void readTemperatureHumidity(){
@@ -380,7 +174,8 @@ void readTemperatureHumidity(){
     Serial.println(F("Error reading temperature!"));
   }
   else {
-    tempTemp = roundUpDecimal(event.temperature);
+    tempTemp = roundUpDecimal(event.temperature-1.2);
+    Serial.println("Temperature: " + String(tempTemp));
   }
   
   dht22.humidity().getEvent(&event);
@@ -388,18 +183,25 @@ void readTemperatureHumidity(){
     Serial.println(F("Error reading humidity!"));
   }
   else {
-    tempHumi = roundUpDecimal(event.relative_humidity);
+    tempHmd = map(event.relative_humidity, 20.6, 69.5, 40.0, 75.0);
+		tempHmd = roundUpDecimal(tempHmd);
+    Serial.println("Humidity: " + String(tempHmd));
   }
 }
 
 void readLight() {
   tempLight = roundUpDecimal(bh1750.readLightLevel());
+  Serial.println("Light Level: " + String(tempLight));
 }
 
 
 void readAtmosphere(){
   tempAtom = roundUpDecimal(bmp280.readPressure()/100);
-  tempAlit = roundUpDecimal(bmp280.readAltitude(currentWeather.pressure));
+  // approx low from https://vancouver.weatherstats.ca/charts/pressure_sea-hourly.html
+  tempAlit = roundUpDecimal(bmp280.readAltitude(1021.1));
+  
+  Serial.println("Pressure: " + String(tempAtom));
+  Serial.println("Altitude: " + String(tempAlit));
 }
 
 //upload temperature humidity data to thinkspak.com
@@ -409,11 +211,12 @@ void uploadSensorData(){
     return;
   }
   // Three values(field1 field2 field3 field4) have been set in thingspeak.com 
-  client.print(String("GET ") + "/update?api_key="+api_key+"&field1="+tempTemp+"&field2="+tempHumi+"&field3="+tempLight+"&field4="+tempAtom+"&field5="+tempAlit+" HTTP/1.1\r\n" +"Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+  client.print(String("GET ") + "/update?api_key="+api_key+"&field1="+tempTemp+"&field2="+tempHmd+"&field3="+tempLight+"&field4="+tempAtom+"&field5="+tempAlit+" HTTP/1.1\r\n" +"Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
   while(client.available()){
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
+  Serial.println("Updated ThingSpeak");
 }
 
 
