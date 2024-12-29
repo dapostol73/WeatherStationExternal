43ce8dac6212e15a941c66c38b91bf8e6fac5815
Demetrius Apostolopoulos <dapostol73@hotmail.com>
2024-12-28 21:24:26 -0800

    Added channel ID for Thingspeak.h

--- src/ThingSpeakInfo.h
+++ src/ThingSpeakInfo.h
@@ -9,13 +9,15 @@ struct ThingSpeakInfo
     const char *Host = "api.thingspeak.com"; //IP address of the thingspeak server
     const char *APIKeyWrite = ""; //Your own thingspeak write api_key
     const char *APIKeyRead = "";	 //Your own thingspeak read api_key
+    unsigned long ChannelID;
 
     ThingSpeakInfo() = default;
 
-    ThingSpeakInfo(const char* apiKeyWrite, const char* apiKeyRead)
+    ThingSpeakInfo(const char* apiKeyWrite, const char* apiKeyRead, unsigned long channelID)
     {
         APIKeyWrite = apiKeyWrite;
         APIKeyRead = apiKeyRead;
+        ChannelID = channelID;
     }
 };
 
--- src/UserSettings.h
+++ src/UserSettings.h
@@ -3,8 +3,8 @@
 
 #include "ApplicationSettings.h"
 
-ApplicationSettings Home(WiFiConnection("homessid", "homepw123"), ThingSpeakInfo("homeWriteAPIKey", "homeReadAPIKey"));
-ApplicationSettings Office(WiFiConnection("officessid", "officepw123"), ThingSpeakInfo("officeWriteAPIKey", "officeReadAPIKey"));
+ApplicationSettings Home(WiFiConnection("homessid", "homepw123"), ThingSpeakInfo("homeWriteAPIKey", "homeReadAPIKey", 1234567));
+ApplicationSettings Office(WiFiConnection("officessid", "officepw123"), ThingSpeakInfo("officeWriteAPIKey", "officeReadAPIKey", 1234567));
 
 ApplicationSettings AppSettings[] = { Home, Office };
 uint8_t AppSettingsCount = sizeof(AppSettings) / sizeof(AppSettings[0]);
